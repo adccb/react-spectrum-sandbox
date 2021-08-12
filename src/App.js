2ab7c42c351ccf3ced100ce185848546f4da4de7
autumn crossan forsythe <acrossan@kyruus.com>
2021-08-12 12:21:26 -0400

    initial commit sandbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import {
+  Provider,
+  defaultTheme,
+  ComboBox,
+  Section,
+  Item,
+  Text,
+} from "@adobe/react-spectrum";
+
+// a sample return from the search service
+const defaultItems = [
+  {
+    name: "Section One",
+    children: [
+      { name: "this is the first item" },
+      { name: "this is the second item" },
+      { name: "this is the third item" },
+    ],
+  },
+];
+
+const App = () => {
+  // store items in local state for now; this will eventually be an xstate context
+  const [items, setItems] = useState([]);
+
+  // this loading state stuff can be taken right from xstate in a hook;
+  // it exists here solely to run the spinner in the right of the input field.
+  const [loadingState, setLoadingState] = useState("idle");
+
+  // this is a stand-in for the fetch or axios call to search
+  const performSearch = () =>
+    setTimeout(() => {
+      setItems(defaultItems);
+      setLoadingState("idle");
+    }, 2000);
+
+  const handleOpenChange = (isOpen) => {
+    if (isOpen) {
+      setLoadingState("loading");
+      performSearch();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Provider theme={defaultTheme}>
+        <ComboBox
+          label="this is a dropdown."
+          loadingState={loadingState}
+          onOpenChange={handleOpenChange}
+          defaultItems={items}
         >
-          Learn React
-        </a>
-      </header>
+          {({ name, children }) => (
+            <Section key={name} items={children} title={name}>
+              {(item) => (
+                <Item key={item.name} textValue={item.name}>
+                  <Text>{item.name}</Text>
+                  <Text slot="description">in {name}</Text>
+                </Item>
+              )}
+            </Section>
+          )}
+        </ComboBox>
+      </Provider>
     </div>
   );
-}
+};
 
 export default App;
